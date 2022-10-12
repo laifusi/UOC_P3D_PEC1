0e5ad4fa4bba4155a99f14ee059b1a18f2a6c1fe
Laia <laia.fustercorral@gmail.com>
2022-10-12 21:51:49 +0200

    Initial Ghost Data Management

---
+++ PEC1_CarrerasContrarreloj/Assets/Scripts/LapData.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class LapData : ScriptableObject
+{
+    List<Vector3> positions;
+    List<Quaternion> rotations;
+
+    public void AddNewData(Transform transform)
+    {
+        positions.Add(transform.position);
+        rotations.Add(transform.rotation);
+    }
+
+    public void AddNewData(Vector3 position, Quaternion rotation)
+    {
+        positions.Add(position);
+        rotations.Add(rotation);
+    }
+
+    public void GetDataAt(int sample, out Vector3 position, out Quaternion rotation)
+    {
+        position = positions[sample];
+        rotation = rotations[sample];
+    }
+
+    public void Reset()
+    {
+        positions.Clear();
+        rotations.Clear();
+    }
+
+}
