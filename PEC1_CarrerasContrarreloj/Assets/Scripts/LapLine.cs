0d778e591c9dc138e55112d21bacad890f900cd1
Laia <laia.fustercorral@gmail.com>
2022-10-23 21:04:47 +0200

    Time Texts added: current lap, first lap, second lap, third lap and best lap

--- PEC1_CarrerasContrarreloj/Assets/Scripts/LapLine.cs
+++ PEC1_CarrerasContrarreloj/Assets/Scripts/LapLine.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LapLine : MonoBehaviour
+public class LapLine : Singleton<LapLine>
 {
     [SerializeField] private int numberOfLaps = 3;
 
@@ -12,8 +12,18 @@ public class LapLine : MonoBehaviour
     private float currentLapEndTime;
     private float bestTime;
 
-    public static event Action<bool, bool> OnNewLap;
-    public static event Action OnNewBestLap;
+    public static Action<bool, bool> OnNewLap;
+    public static Action OnNewBestLap;
+    public static Action<TypeOfLapText, float> OnCurrentTimeUpdate;
+    public static Action<TypeOfLapText, float> OnFixedTimeUpdate;
+
+    private void Update()
+    {
+        if(currentLapId != 0 && currentLapId <= numberOfLaps)
+        {
+            OnCurrentTimeUpdate?.Invoke(TypeOfLapText.CurrentLap, Time.unscaledTime - currentLapStartTime);
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -24,11 +34,14 @@ public class LapLine : MonoBehaviour
             {
                 currentLapEndTime = Time.unscaledTime;
 
+                OnFixedTimeUpdate?.Invoke(GetFinishedLapTypeText(currentLapId), currentLapEndTime - currentLapStartTime);
+
                 if(bestTime == 0 || currentLapEndTime - currentLapStartTime < bestTime)
                 {
                     Debug.Log("New Best Time!");
                     bestTime = currentLapEndTime - currentLapStartTime;
                     OnNewBestLap?.Invoke();
+                    OnFixedTimeUpdate?.Invoke(TypeOfLapText.BestLap, bestTime);
                 }
             }
 
@@ -43,5 +56,20 @@ public class LapLine : MonoBehaviour
                 GetComponent<Collider>().enabled = false;
             }
         }
+    }
+
+    private TypeOfLapText GetFinishedLapTypeText(int currentLapId)
+    {
+        switch(currentLapId)
+        {
+            case 2:
+                return TypeOfLapText.FirstLap;
+            case 3:
+                return TypeOfLapText.SecondLap;
+            case 4:
+                return TypeOfLapText.ThirdLap;
+        }
+
+        return TypeOfLapText.Invalid;
     }
 }
---
+++ PEC1_CarrerasContrarreloj/Assets/Scripts/UI/UILapTimes.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UILapTimes : MonoBehaviour
+{
+    [SerializeField] TypeOfLapText type;
+
+    private TMP_Text text;
+    private int hours;
+    private int minutes;
+    private float seconds;
+    private string time;
+
+    private void Start()
+    {
+        if(type == TypeOfLapText.CurrentLap)
+        {
+            LapLine.OnCurrentTimeUpdate += UpdateTime;
+        }
+        else
+        {
+            LapLine.OnFixedTimeUpdate += UpdateTime;
+        }
+
+        text = GetComponent<TMP_Text>();
+        text.enabled = false;
+    }
+
+    private void UpdateTime(TypeOfLapText type, float timeInSeconds)
+    {
+        if(this.type == type)
+        {
+            hours = (int)timeInSeconds / 3600;
+            minutes = (int)(timeInSeconds - hours*3600) / 60;
+            seconds = timeInSeconds - hours * 3600 - minutes * 60;
+            time = hours > 9 ? hours.ToString() : '0' + hours.ToString();
+            time += ':';
+            time += minutes > 9 ? minutes.ToString() : '0' + minutes.ToString();
+            time += ':';
+            time += seconds > 9 ? seconds.ToString("F2") : '0' + seconds.ToString("F2");
+            text.SetText(time);
+            text.enabled = true;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (type == TypeOfLapText.CurrentLap)
+        {
+            LapLine.OnCurrentTimeUpdate -= UpdateTime;
+        }
+        else
+        {
+            LapLine.OnFixedTimeUpdate -= UpdateTime;
+        }
+    }
+}
+
+public enum TypeOfLapText
+{
+    CurrentLap, BestLap, FirstLap, SecondLap, ThirdLap, Invalid
+}
